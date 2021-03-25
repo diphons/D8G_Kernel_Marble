ab32a3f343d32787a920b23fe625f389d574e366
Paul E. McKenney <paulmck@kernel.org>
2021-03-24 17:20:18 -0700

    Merge branches 'bitmaprange.2021.03.08a', 'fixes.2021.03.15a', 'kvfree_rcu.2021.03.08a', 'mmdumpobj.2021.03.08a', 'nocb.2021.03.15a', 'poll.2021.03.24a', 'rt.2021.03.08a', 'tasks.2021.03.08a', 'torture.2021.03.08a' and 'torturescript.2021.03.22a' into HEAD
    bitmaprange.2021.03.08a:  Allow 3-N for bitmap ranges.
    fixes.2021.03.15a:  Miscellaneous fixes.
    kvfree_rcu.2021.03.08a:  kvfree_rcu() updates.
    mmdumpobj.2021.03.08a:  mem_dump_obj() updates.
    nocb.2021.03.15a:  RCU NOCB CPU updates, including limited deoffloading.
    poll.2021.03.24a:  Polling grace-period interfaces for RCU.
    rt.2021.03.08a:  Realtime-related RCU changes.
    tasks.2021.03.08a:  Tasks-RCU updates.
    torture.2021.03.08a:  Torture-test updates.
    torturescript.2021.03.22a:  Torture-test scripting updates.

--- mm/slub.c
+++ mm/slub.c
@@ -3990,6 +3990,7 @@ int __kmem_cache_shutdown(struct kmem_cache *s)
 	return 0;
 }
 
+#ifdef CONFIG_PRINTK
 void kmem_obj_info(struct kmem_obj_info *kpp, void *object, struct page *page)
 {
 	void *base;
@@ -4029,6 +4030,7 @@ void kmem_obj_info(struct kmem_obj_info *kpp, void *object, struct page *page)
 #endif
 #endif
 }
+#endif
 
 /********************************************************************
  *		Kmalloc subsystem
