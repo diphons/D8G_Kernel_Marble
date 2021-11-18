2d899e05ed1350c5558786f6d7b6611b74182321
Andrzej Perczak <linux@andrzejperczak.com>
2021-11-18 16:26:03 +0100

    kernel: mute process no longer affine to cpu
    Signed-off-by: Andrzej Perczak <linux@andrzejperczak.com>
    Change-Id: I7bb0eac60af19ba6d337f6b41d5d05ee04d09e3e

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -2546,7 +2546,7 @@ out:
 		 * leave kernel.
 		 */
 		if (p->mm && printk_ratelimit()) {
-			printk_deferred("process %d (%s) no longer affine to cpu%d\n",
+			pr_debug("process %d (%s) no longer affine to cpu%d\n",
 					task_pid_nr(p), p->comm, cpu);
 		}
 	}
