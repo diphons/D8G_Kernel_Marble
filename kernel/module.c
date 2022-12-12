f4d65da50063ae8ed62590cbf91f094e4cfc076b
zzh-zzh-zzh <1292882190@qq.com>
2022-12-12 13:49:59 +0800

    Kernel/module.c: Abort modules check
    Change-Id: I415edff425118f0c3a510f69a758ee15df0ba7f3

--- kernel/module.c
+++ kernel/module.c
@@ -1353,8 +1353,6 @@ static int check_version(const struct load_info *info,
 	return 1;
 
 bad_version:
-	pr_warn("%s: disagrees about version of symbol %s, but ignore...\n",
-	       info->name, symname);
 	return 1;
 }
 
