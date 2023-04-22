f64d35b25fba578cfb6c8c0c05df129978e68d28
TheVoyager0777 <l727127746@outlook.com>
2023-04-23 01:05:00 +0800

    Introduce Lz4k
    Change-Id: Ia9824427dddcc7ddb379d0f1864624288d4ef053

--- mm/oplus_mm/hybridswap_zram/zcomp.c
+++ mm/oplus_mm/hybridswap_zram/zcomp.c
@@ -32,6 +32,9 @@ static const char * const backends[] = {
 #if IS_ENABLED(CONFIG_CRYPTO_ZSTDN)
 	"zstdn",
 #endif
+#if IS_ENABLED(CONFIG_CRYPTO_LZ4K)
+	"lzK",
+#endif
 };
 
 static void zcomp_strm_free(struct zcomp_strm *zstrm)
