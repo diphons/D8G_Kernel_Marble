94cbeb2af02908ae780b6a39fcfb5502af9e5ab5
diphons <diphons@gmail.com>
2023-07-17 17:11:02 +0700

    Revert some useless log and debug drivers from blacklist Won't boot on MIUI

--- kernel/module.c
+++ kernel/module.c
@@ -3563,10 +3563,6 @@ static char *custom_module_blacklist[] = {
 #if defined(CONFIG_BOARD_INGRESS)
 	"xiaomi_touch",
 #endif
-    /* Useless logs */
-    "cameralog", "f_fs_ipc_log",
-    /* Debug */
-    "qcom_cpufreq_hw_debug", "qcom_iommu_debug", "qti_battery_debug", "rdbg", "spmi_glink_debug", "spmi_pmic_arb_debug",
     /* Coresight */
     "coresight", "coresight_csr", "coresight_cti", "coresight_dummy", "coresight_funnel",
     "coresight_hwevent", "coresight_remote_etm", "coresight_replicator", "coresight_stm",
