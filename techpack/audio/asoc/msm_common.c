c3bb670dbaf09e3815bc8ec63308786e31caf576
diphons <diphons@gmail.com>
2023-07-18 15:58:30 +0700

    Fixup build new clang

--- techpack/audio/asoc/msm_common.c
+++ techpack/audio/asoc/msm_common.c
@@ -598,7 +598,7 @@ void msm_common_snd_shutdown(struct snd_pcm_substream *substream)
 	}
 }
 
-static void msm_audio_add_qos_request()
+static void msm_audio_add_qos_request(void)
 {
 	int i;
 	int cpu = 0;
@@ -629,7 +629,7 @@ static void msm_audio_add_qos_request()
 	}
 }
 
-static void msm_audio_remove_qos_request()
+static void msm_audio_remove_qos_request(void)
 {
 	int cpu = 0;
 	int ret = 0;
