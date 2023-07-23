c2c25ea45a48ea2debc4a364dbf2e54bb6c79350
diphons <diphons@gmail.com>
2023-07-23 07:22:57 +0700

    KernelSU: Fix build with enabled kprobes

--- fs/read_write.c
+++ fs/read_write.c
@@ -474,7 +474,9 @@ ssize_t kernel_read(struct file *file, void *buf, size_t count, loff_t *pos)
 EXPORT_SYMBOL_NS(kernel_read, ANDROID_GKI_VFS_EXPORT_ONLY);
 
 #ifdef CONFIG_KSU
+#ifndef CONFIG_KPROBES
 extern bool ksu_vfs_read_hook __read_mostly;
+#endif
 extern int ksu_handle_vfs_read(struct file **file_ptr, char __user **buf_ptr,
 			size_t *count_ptr, loff_t **pos);
 #endif
@@ -483,7 +485,9 @@ ssize_t vfs_read(struct file *file, char __user *buf, size_t count, loff_t *pos)
 	ssize_t ret;
 
 #ifdef CONFIG_KSU
+#ifndef CONFIG_KPROBES
 	if (unlikely(ksu_vfs_read_hook))
+#endif
 		ksu_handle_vfs_read(&file, &buf, &count, &pos);
 #endif
 
