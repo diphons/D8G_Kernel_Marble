c7d919d36888869803e0335230658588bce25073
Rudi Setiyawan <diphons@gmail.com>
2024-02-20 09:26:56 +0700

    kernel: modue: Builtin touchscreen Signed-off-by: Rudi Setiyawan <diphons@gmail.com>

--- kernel/module.c
+++ kernel/module.c
@@ -3586,6 +3586,8 @@ static char *custom_module_blacklist[] = {
 static char *custom_module_blacklist_marble[] = {
     /* Not required */
     "qca6750", "icnss2", "cs35l41_dlkm", "atmel_mxt_ts", "focaltech_fts", "nt36xxx_i2c", "nt36xxx_spi", "synaptics_dsx",
+    /* Xiaomi Touch */
+	"xiaomi_touch", "goodix_core",
     /* Useless logs */
     "cameralog", "f_fs_ipc_log",
     /* Debug */
